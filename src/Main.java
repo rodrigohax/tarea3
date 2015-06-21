3167661f3b6ea0ff3c12cda1ac93ba7e593ac231
Rodrigo <rodrigohax@gmail.com>
2015-06-20 21:21:09 -0300

    yolo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,40 @@
 public class Main {
     public static void main(String[] args) {
         Archivo archivo = new Archivo();
-        archivo.imprimir();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elige tu opción kenita");
+        System.out.println("1.-Ingresar datos\n2.-Imprimir");
+        int opcion = sc.nextInt();
+        do {
+            switch(opcion){
+                case 1:
+                    System.out.println("Ingresa el codigo");
+                    int codigo = sc.nextInt();
+                    System.out.println("Ingresa isbn");
+                    String isbn = sc.next();
+                    System.out.println("Ingrese autor");
+                    String autor = sc.next();
+                    System.out.println("Ingrese titulo");
+                    String titulo = sc.next();
+                    System.out.println("Ingrese año");
+                    int anho = sc.nextInt();
+                    System.out.println("Ingrese editorial");
+                    String editorial = sc.next();
+                    System.out.println("Ingrese Materia");
+                    String materia = sc.next();
+                    System.out.println("Ingrese copias");
+                    int copias = sc.nextInt();
+                    System.out.println("Ingrese estado true/false");
+                    boolean estado = sc.nextBoolean();
+                    archivo.escribirLibro(new Libro(codigo,isbn,autor,titulo,anho,editorial,materia,
+                    copias,estado));
+                    break;
+                case 2:
+                    archivo.imprimir();
+                    break;
+                default: System.out.println("Kenita tonta");
+            }
+        } while (opcion==3);
+        
     }
 }
