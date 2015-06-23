238a30ff8feae97cec29b2a0083e290574d6ab82
Rodrigo <rodrigohax@gmail.com>
2015-06-22 22:45:26 -0300

    added hashing functions

--- src/HashTable.java
+++ src/HashTable.java
@@ -3,43 +3,60 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
  * @author Rodrigo
  */
-public class HashTable{
+public class HashTable {
+
     private Libro hashArray[];
     private int arraySize;
     private Libro nonItem;
-    
-    public HashTable(int size){
+
+    public HashTable(int size) {
         arraySize = size;
         hashArray = new Libro[arraySize];
         nonItem = new Libro();//constructor sin argumentos ya que es nulo
     }
-    
-    public void displayTable(){
+
+    public void displayTable() {
         System.out.println("Tabla");
-        for(int i=0;i<arraySize;i++){
-            if(hashArray[i]!=null){
+        for (int i = 0; i < arraySize; i++) {
+            if (hashArray[i] != null) {
                 System.out.print(hashArray[i].getCodigo());
-            }else{
+            } else {
                 System.out.print("** ");
             }
             System.out.println("");
         }
     }
-    public int hashFunc(int key){
+
+    public int hashFunc(int key) {
         return key % arraySize;
     }
-    
-    public void insert(Libro item){
+
+    public void insert(Libro item) {
         int key = item.getCodigo();
         int hashVal = hashFunc(key);
-        while(hashArray[hashVal]!=null && hashArray[hashVal].getCodigo()==-1){
+        while (hashArray[hashVal] != null && hashArray[hashVal].getCodigo() == -1) {
             ++hashVal;
             hashVal %= arraySize;
         }
         hashArray[hashVal] = item;
+    }
+
+    public Libro delete(int key){
+        int hashVal = hashFunc(key);
+        while (hashArray[hashVal] != null){
+            if (hashArray[hashVal].getCodigo() == key){
+                Libro temp = hashArray[hashVal];
+                hashArray[hashVal] = nonItem;
+                return temp;
+            }
+            ++hashVal;
+            hashVal %= arraySize;
+        }
+        return null;
     }
 }
